115c17a31e70b08ef93c5a12851bc9b8c7b84ed3
Paulier <paulier.r.c@gmail.com>
2023-03-26 17:45:39 -0300

    Projeto controle financeiro HTML e CSS atualizado

--- 3_projetos/4_controleFinanceiro/script.js
+++ 3_projetos/4_controleFinanceiro/script.js
@@ -1,8 +1,15 @@
-function calcular(){
-    let receita = Number(document.getElementById('txreceita').value)
-    let despesa = document.getElementById('txdespesa')
-    res = document.querySelector('div#res')
-    
+const boxLancamento = document.querySelector('.boxLançamento')
+const lancamento = Number(document.querySelector('.textinput').value)
+const novaReceita = document.querySelector('.BttReceita')
+const novaDespesa = document.querySelector('.BttDespesa')
+const tabela = document.querySelector(".tabela")
 
+novaReceita.addEventListener('click', function adiciobaDespesa(){
+    const totValores = []
+    totValores.push(lancamento)
+    tabela.innerHTML =+ `${totValores}`
+})
 
-}
+novaDespesa.addEventListener('click', function adicionaReceita (){
+    tabela.innerText =+`${totValores}`
+})
