8e0227e2835fd0445031216275aa27974708a483
Paulier <paulier.r.c@gmail.com>
2023-04-06 18:52:12 -0300

    Aula 076 adicionada, calculadora com Factory Function

--- 3_projetos/4_controleFinanceiro/script.js
+++ 3_projetos/4_controleFinanceiro/script.js
@@ -1,15 +1,40 @@
-const boxLancamento = document.querySelector('.boxLançamento')
-const lancamento = Number(document.querySelector('.textinput').value)
-const novaReceita = document.querySelector('.BttReceita')
-const novaDespesa = document.querySelector('.BttDespesa')
-const tabela = document.querySelector(".tabela")
-const totValores = []
+const totalLancamentos = document.querySelector(".totvalor")
+const totValor = document.querySelector(".totvalor")
+const novoLancamento = document.querySelector(".boxLançamento")
+let BttReceita = document.querySelector(".BttReceita")
+const BttDespesa = document.querySelector(".BttDespesa")
+const totReceitas = document.querySelector(".totReceitas")
+const totDespesas = document.querySelector(".totDespesas")
+let tabela = document.querySelector(".tabela")
+let novaLinha = []
 
-novaReceita.addEventListener('click', function adiciobaDespesa(){
-    totValores.push(lancamento)
-    tabela.innerHTML =+ `${totValores}`
-})
+function iniciaControle() {
 
-novaDespesa.addEventListener('click', function adicionaReceita (){
-    tabela.innerText =+`${totValores}`
-})
+
+    adcionarReceita = () => {
+        let textInput = Number(document.querySelector(".textinput").value)
+        novaLinha.push(textInput)
+        let adicionarLinha = document.createElement('td')
+        tabela.appendChild(adicionarLinha)        
+        tabela.innerHTML += `Nova Receita adicionada R$ ${novaLinha}`
+        
+    }
+    adcionarDespesa = () => {
+        let textInput = Number(document.querySelector(".textinput").value)
+        novaLinha.push(textInput)
+        let adicionarLinha = document.createElement('td')
+        tabela.appendChild(adicionarLinha)
+        tabela.innerHTML += `Nova Despesa adicionada R$ ${novaLinha}`
+    }
+    balanco = () => {
+        let totBalanco = document.createElement('p')
+        totValor.appendChild(totBalanco)
+        totValor.innerHTML += `${novaLinha}`
+    
+        }
+ 
+}
+
+iniciaControle()
+
+
